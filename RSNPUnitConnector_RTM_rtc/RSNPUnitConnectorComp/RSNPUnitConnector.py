54cc694de58f18ed18e38ec44562eed726ae7a1c
SatoshiOkano <44587055+SatoshiOkano@users.noreply.github.com>
2019-11-06 16:52:27 +0900

    fix

--- RSNPUnitConnector_RTM_rtc/RSNPUnitConnectorComp/RSNPUnitConnector.py
+++ RSNPUnitConnector_RTM_rtc/RSNPUnitConnectorComp/RSNPUnitConnector.py
@@ -89,7 +89,6 @@ class RSNPUnitConnector(OpenRTM_aist.DataFlowComponentBase):
 		 - DefaultValue: 8000
 		"""
         self._Port = [8000]
-        #############要変更#############
 
         self._Sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         # </rtc-template>
@@ -113,37 +112,31 @@ class RSNPUnitConnector(OpenRTM_aist.DataFlowComponentBase):
         return RTC.RTC_OK
 
     def onActivated(self, ec_id):
-        print("RSNPUnitConnectorRTC activate")
+        print("activate")
         print("now connecting to RSNPUnit")
         self._Sock.connect((self._IPaddress[0], self._Port[0]))
         print("connecting success")
-        # failした場合 error
         return RTC.RTC_OK
 
     def onDeactivated(self, ec_id):
-        print("RSNPUnitConnectorRTC deactivate")
+        print("deactivate")
         print("now disconnect to RSNPUnit")
         self._Sock.close()
         print("disconnect success")
-        # failした場合 error
         return RTC.RTC_OK
 
     def onExecute(self, ec_id):
 
-        
         if self._SampleDataInIn.isNew():
-            read_data = self._SampleDataInIn.read()
-            rcv_data_str = str(read_data)
+            self._d_SampleDataIn = self._SampleDataInIn.read()
+            rcv_data_str = str(self._d_SampleDataIn.data)
             print("recieve data: " + rcv_data_str)  # to debug
-            send_data_json = {"data": [
-                {"ac_id": 1, "ac": "robot state", "re_id": 1, "re": rcv_data_str, "co": ""}]}
-            # json format
+            #send_data_json = {"data": [
+            #    {"ac_id": 1, "ac": "robot state", "re_id": 1, "re": rcv_data_str, "co": ""}]}
+            #send_data_str = json.dumps(send_data_json)
 
-            # 出来ればサービスポートで定義する
-            send_data_str = json.dumps(send_data_json)
-
-            self._Sock.send(send_data_str)
-            print("send data by socket: " + send_data_str)
+            self._Sock.send(rcv_data_str)
+            print("send data by socket: " + rcv_data_str)
         time.sleep(2)
         return RTC.RTC_OK
 
